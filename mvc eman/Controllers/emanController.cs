3ba3f74694397ceeeadd2a3f929afe788971f81d
emankamalmohamed <44709765+emankamalmohamed@users.noreply.github.com>
2018-11-02 23:59:46 +0200

    initial

---
+++ mvc eman/Controllers/emanController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace mvceman.Controllers
+{
+    public class emanController : Controller
+    {
+        
+
+
+        public string Index()
+        {
+            return "Hello my name is eman";
+        }
+        
+
+       
+        public string Welcome()
+        {
+            return "This is my welcome page";
+        }
+    }
+}
